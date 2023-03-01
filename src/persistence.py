1ab2a1122972eae278a2f8d33af5520cc50e93b3
Ivan Mikhailov <mikhailov.ivan@gmail.com>
2023-03-01 16:47:51 +0300

    Initial version

---
+++ src/persistence.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def mean_cos(k: float) -> float:
+    if k == 0.0:
+        return 0
+    else:
+        return (np.exp(-2*k)*(np.exp(2*k)*(k-1)+k+1) / k) / (1-np.exp(-2*k))
+
+def persistence_length(k: float, length_bond: float) -> float:
+    return length_bond / abs(np.log(mean_cos(k)))
+
+def kuhn_segment(k: float, length_bond: float) -> float:
+    return length_bond * (1+mean_cos(k)) / (1-mean_cos(k))
+
+if __name__ == '__main__':
+    k = np.arange(0.0, 20.5, 0.5)
+    l_k = np.zeros(len(k))
+    l_p = np.array([persistence_length(x, length_bond=1.0) for x in k])
+    l_k = np.array([kuhn_segment(x, length_bond=1.0) for x in k])
+    plt.plot(k, l_p)
+    plt.savefig('l_p.jpg')
+    
